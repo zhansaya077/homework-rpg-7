c316e062ab5e0bb80cbcadfd4fe99ff6e63046a5
zhansaya077 <zhansaya.bekbulat@narxoz.kz>
2026-04-16 20:22:59 +0500

    Refactor hero and boss initialization in Main.java

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -4,14 +4,8 @@ import com.narxoz.rpg.combatant.DungeonBoss;
 import com.narxoz.rpg.combatant.Hero;
 import com.narxoz.rpg.engine.DungeonEngine;
 import com.narxoz.rpg.engine.EncounterResult;
-import com.narxoz.rpg.observer.AchievementTracker;
-import com.narxoz.rpg.observer.BattleLogger;
-import com.narxoz.rpg.observer.HeroStatusMonitor;
-import com.narxoz.rpg.observer.LootDropper;
-import com.narxoz.rpg.observer.PartySupport;
-import com.narxoz.rpg.strategy.AggressiveStrategy;
-import com.narxoz.rpg.strategy.BalancedStrategy;
-import com.narxoz.rpg.strategy.DefensiveStrategy;
+import com.narxoz.rpg.observer.*;
+import com.narxoz.rpg.strategy.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,58 +14,36 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Hero warrior = new Hero(
-                "Warrior",
-                140,
-                30,
-                10,
-                new AggressiveStrategy()
-        );
-
-        Hero tank = new Hero(
-                "Tank",
-                200,
-                18,
-                20,
-                new DefensiveStrategy()
-        );
-
-        Hero rogue = new Hero(
-                "Rogue",
-                110,
-                25,
-                8,
-                new BalancedStrategy()
-        );
+        Hero warrior = new Hero("Warrior", 150, 30, 10, new AggressiveStrategy());
+        Hero tank = new Hero("Tank", 220, 18, 25, new DefensiveStrategy());
+        Hero rogue = new Hero("Rogue", 120, 25, 8, new BalancedStrategy());
 
         List<Hero> heroes = new ArrayList<>();
         heroes.add(warrior);
         heroes.add(tank);
         heroes.add(rogue);
 
-        DungeonBoss boss = new DungeonBoss(
-                "Cursed Dragon",
-                500,
-                35,
-                12
-        );
+        DungeonBoss boss = new DungeonBoss("Cursed Dragon", 600, 35, 12);
 
         BattleLogger logger = new BattleLogger();
         AchievementTracker tracker = new AchievementTracker();
         PartySupport support = new PartySupport(heroes);
         HeroStatusMonitor monitor = new HeroStatusMonitor();
-        LootDropper lootDropper = new LootDropper();
+        LootDropper loot = new LootDropper();
 
         boss.registerObserver(logger);
         boss.registerObserver(tracker);
         boss.registerObserver(support);
         boss.registerObserver(monitor);
-        boss.registerObserver(lootDropper);
+        boss.registerObserver(loot);
         boss.registerObserver(boss);
 
         DungeonEngine engine = new DungeonEngine();
 
         EncounterResult result = engine.runEncounter(heroes, boss);
+
+        System.out.println(warrior.getName() + " switches strategy during battle.");
+        warrior.setStrategy(new DefensiveStrategy());
 
         System.out.println(result);
     }
